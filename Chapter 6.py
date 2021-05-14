9b1f638f41075151dc49c615eaacd93195dd06db
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2021-05-14 22:48:08 +0300

    first commit

---
+++ Chapter 6.py
@@ -0,0 +1,27 @@
+#Chapter 6: DEFINING FUNCTIONS
+
+#No 1
+
+print('This program prints the lyrics of Old MacDonald')
+
+def firstLine():
+    return 'Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!'
+
+def Eeigh():
+    return 'Ee-igh, Ee-igh, Oh!'
+
+def secondLine(animal):
+    second = 'And on that farm he had a'+ animal +,
+    return 'And on that farm he had a cow, Ee-igh, Ee-igh, Oh!'
+
+
+
+def happy():return "Happy Birthday to you!\n"
+
+def verseFor(person):191lyrics = happy0*2 + "Happy birthday, dear " + person + ". \n" + happy()
+
+    return lyrics
+def main():
+    for person in ["Fred", "Lucy", "Elmer"]:
+        
+    print(verseFor(person))main()
