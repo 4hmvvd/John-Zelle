57bb50d073e40fb712f79d6548f85eb3f1e2950a
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2021-05-30 23:54:12 +0300

    in progress

--- Chapter 8/Chapter 8.py
+++ Chapter 8/Chapter 8.py
@@ -2,4 +2,19 @@
 
 #No 1
 
-print('')
+print('This program computes the nth term of a Fibonacci series')
+
+nth = int(input('Enter a nth term of the Fibonacci series you want: '))
+
+def fibo(nth):
+
+    a, b = 0, 1
+
+    for i in range(3,nth+1):
+        c = a + b
+        a = b
+        b = c
+
+    print(c)
+
+fibo(nth)
