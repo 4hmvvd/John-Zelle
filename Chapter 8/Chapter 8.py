1a035f910eff2be0a1eb82807b8b51d38feb20f4
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2021-09-06 22:39:12 +0300

    in prog

--- Chapter 8/Chapter 8.py
+++ Chapter 8/Chapter 8.py
@@ -95,7 +95,7 @@ while x > 1:
     x = syrX
     print(int(syrX),end=' ')
 
-'''
+
 #No 5
 import math as m
 
@@ -128,7 +128,39 @@ else:
             i = i + 1
             
     print('\nThe number is prime.')
-
+    
+'''
 #No 6
 
-    #####
+import math as m
+
+def primeNum(n):
+
+        #check if n is evenly divisible by values between 2 and int(m.sqrt(n))
+        x = m.sqrt(n)
+        i = 2
+
+        while i <= x:
+            value = n % i
+            #if any value is divisble, break and close program
+            if value == 0:
+                return
+            #if no value is divisible evenly, print("The number is prime.")
+            else:
+                i = i + 1
+        print(n)
+
+def main():
+    x = eval(input("Input a positive whole number: "))
+    #Validate x as a positive whole number
+    if x == 0:
+        x = eval(input("Your number must be higher than 0: "))
+    elif (x % 1 != 0):
+        x = eval(input("The number you entered was not whole: "))
+    elif (x < 1):
+        x = eval(input("The number you entered was not positive: "))
+    else:
+        for n in range(x):
+            primeNum(n+1)
+
+main()
